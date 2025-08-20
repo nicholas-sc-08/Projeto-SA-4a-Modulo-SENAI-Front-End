b31056947dfce0d40f96003fcfd3b6f3b9fc7910
nicholas-sc-08 <nicholassc.2008@gmail.com>
2025-08-20 16:53:15 -0300

    feat: barra de pesquisar conversas no chat

--- front_end/src/components/Chat/Informações/Informacoes.jsx
+++ front_end/src/components/Chat/Informações/Informacoes.jsx
@@ -10,8 +10,9 @@ function Informacoes() {
   const { array_chat, set_array_chat } = useContext(GlobalContext);
   const { usuario_logado, set_usuario_logado } = useContext(GlobalContext);
   const [cliente_brecho, set_cliente_brecho] = useState(null);
-  const [barra_de_pesquisa, set_barra_de_pesquisa] = useState(null);
+  const [barra_de_pesquisa, set_barra_de_pesquisa] = useState(``);
   const [conversas_frequentes, set_conversas_frequentes] = useState([]);
+  const [conversas_filtradas, set_conversas_filtradas] = useState([]);
 
   useEffect(() => {
 
@@ -25,6 +26,16 @@ function Informacoes() {
     const cliente = array_clientes.find(c => c._id == usuario_logado._id);
     cliente ? set_cliente_brecho(`cliente`) : set_cliente_brecho(`brecho`);
   }, []);
+
+  useEffect(() => {
+
+    if (cliente_brecho == `cliente`) {
+
+      const filtrar_contatos = usuario_logado.conversas.filter(contato => contato.nome_brecho.toUpperCase().includes(barra_de_pesquisa.toUpperCase()));
+      set_conversas_filtradas(filtrar_contatos);
+    }
+
+  }, [barra_de_pesquisa]);
 
   async function buscar_clientes() {
 
@@ -108,7 +119,7 @@ function Informacoes() {
       </header>
       <search className="barra_de_pesquisa">
         <img src="./img/icons/Pesquisa.svg" alt="" />
-        <input type="text" placeholder="Procurar por Conversa" />
+        <input type="text" placeholder="Procurar por Conversa" value={barra_de_pesquisa} onChange={e => set_barra_de_pesquisa(e.target.value)} />
       </search>
       <section className="container_conversas_frequentes">
         {usuario_logado.conversas ? [usuario_logado.conversas[0], usuario_logado.conversas[1], usuario_logado.conversas[2]].map((conversa, i) => (
@@ -120,18 +131,18 @@ function Informacoes() {
       </section>
       <div className="sub_titulo_conversas_chat">
         {usuario_logado.conversas ?
-          <h4>Recentes({usuario_logado.conversas.length})</h4>
+          <h4>Recentes({conversas_filtradas.length == 0 ? usuario_logado.conversas.length : conversas_filtradas.length})</h4>
           : ``
         }
       </div>
 
       <section className="container_contatos_chat">
-        {usuario_logado.conversas ? usuario_logado.conversas.map((contato, i) => (
+        {usuario_logado.conversas.length > 0 && barra_de_pesquisa == `` ? usuario_logado.conversas.map((contato, i) => (
           <div key={i} className="container_contato_chat">
             <img src={cliente_brecho == `cliente` ? contato.logo : contato.imagem_de_perfil} referrerPolicy="no-referrer" crossOrigin="anonymous" alt="" />
             <aside className="info_principal_contatos_chat">
               <h5>{cliente_brecho == `cliente` ? contato.nome_brecho : contato.nome}</h5>
-              <p>teste</p>
+              <p>testeee</p>
             </aside>
             <aside className="info_horario_contatos_chat">
               <h5>12:06</h5>
@@ -139,6 +150,24 @@ function Informacoes() {
             </aside>
           </div>
         )) : ``}
+        {conversas_filtradas.length > 0 ? conversas_filtradas.map((contato, i) => (
+          <div key={i} className="container_contato_chat">
+            <img src={cliente_brecho == `cliente` ? contato.logo : contato.imagem_de_perfil} referrerPolicy="no-referrer" crossOrigin="anonymous" alt="" />
+            <aside className="info_principal_contatos_chat">
+              <h5>{cliente_brecho == `cliente` ? contato.nome_brecho : contato.nome}</h5>
+              <p>testeee</p>
+            </aside>
+            <aside className="info_horario_contatos_chat">
+              <h5>12:06</h5>
+              <span>3</span>
+            </aside>
+          </div>
+        )) : ``}
+        {conversas_filtradas.length == 0 && barra_de_pesquisa != `` ?
+          <div className="container_nenhuma_conversa_encontrada_chat">
+            <img src="./img/icons/icone_chat_nenhuma_conversa.svg" alt="" />
+            <p>Tentamos procurar por conversas mas parece que não conseguimos encontrar nenhuma conversa!</p>
+          </div> : ``}
       </section>
     </div>
   )
