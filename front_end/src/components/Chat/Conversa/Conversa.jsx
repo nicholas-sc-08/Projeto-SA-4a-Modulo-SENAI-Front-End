d0a0783b7a84db20b19dea1beb2cbcfe6153a974
nicholas-sc-08 <nicholassc.2008@gmail.com>
2025-09-01 17:20:55 -0300

    wip: chat

--- front_end/src/components/Chat/Conversa/Conversa.jsx
+++ front_end/src/components/Chat/Conversa/Conversa.jsx
@@ -2,13 +2,49 @@ import { useState, useContext, useEffect } from "react";
 import { GlobalContext } from '../../../contexts/GlobalContext.jsx';
 import { motion, AnimatePresence } from "framer-motion";
 import './Conversa.css';
+import api from "../../../services/api.js";
 
 function Conversa() {
 
   const { conversa_selecionada, set_conversa_selecionada } = useContext(GlobalContext);
+  const { usuario_logado, set_usuario_logado } = useContext(GlobalContext);
   const { grupo_conversa, set_grupo_conversa } = useContext(GlobalContext);
   const { chat_pagina_atual, set_chat_pagina_atual } = useContext(GlobalContext);
+  const { array_chat, set_array_chat } = useContext(GlobalContext);
   const [inpt_mensagem, set_inpt_mensagem] = useState(``);
+  const [conversa_atual, set_conversa_atual] = useState([]);
+
+  useEffect(() => {
+
+    buscar_chat();
+
+    const mensagens_filtradas = array_chat.filter(mensagem => {
+
+      return mensagem.id_dono_mensagem == usuario_logado._id && mensagem.id_quem_recebeu_mensagem == conversa_selecionada._id || mensagem.id_dono_mensagem == conversa_selecionada._id && mensagem.id_quem_recebeu_mensagem == usuario_logado._id
+    });
+
+    set_conversa_atual(mensagens_filtradas);
+
+  }, []);
+
+  useEffect(() => {
+
+    console.log(conversa_atual);
+
+  }, [conversa_atual]);
+
+  async function buscar_chat() {
+
+    try {
+
+      const mensagens = await api.get(`/chats`);
+      set_array_chat(mensagens.data);
+
+    } catch (erro) {
+
+      console.error(erro);
+    };
+  };
 
   return (
     <AnimatePresence>
@@ -22,6 +58,12 @@ function Conversa() {
             <button><img src="./img/icons/icone_chat_opcoes.svg" /></button>
           </div>
         </header> : ``}
+        {conversa_selecionada ? <section className="container_chat_fundo_da_conversa">
+          {conversa_atual.map((_, i) => {
+
+            <p>{_.mensagem}</p>
+          })}
+        </section> : ``}
         {conversa_selecionada ? <div className="container_interagir_conversa">
           <div className="container_input_chat_conversa">
             <input type="text" placeholder="Digite sua Mensagem..." value={inpt_mensagem} onChange={e => set_inpt_mensagem(e.target.value)} />
