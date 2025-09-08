b9853a93f9300faca0e6cd764aaac21e3279e0fb
NICHOLAS SERENCOVICH CARVALHO <nicholas_carvalho@estudante.sesisenai.org.br>
2025-09-08 17:26:57 -0300

    feat: migrando do vite para o next

--- back_end/models/Estoque.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const EstoqueSchema = new mongoose.Schema({
-
-    quantidade: { type:Number, required: true }, produto_id: {type:String, required:true}
-}, {timestamps: true})
-
-const Estoque = mongoose.model("Estoque", EstoqueSchema);
-module.exports = Estoque
--- back_end/models/Marca.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const MarcaSchema = new mongoose.Schema({
-
-    logoMarca: { type: String, required: true },
-    nome: { type: String, required: true },
-}, { timestamps: true })
-
-const Marca = mongoose.model("Marca", MarcaSchema);
-module.exports = Marca;
