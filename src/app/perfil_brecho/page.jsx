73cd575e2b47662428ef6350ef64a7ae4680b65e
dudawl18 <mwolfluiz123a3@gmail.com>
2025-10-06 00:28:29 -0300

    wip: Tela de Perfil do Brechó em andamento

--- src/app/perfil_brecho/page.jsx
+++ src/app/perfil_brecho/page.jsx
@@ -9,13 +9,49 @@ import styles from "@/app/perfil_brecho/page.module.css";
 
 function page() {
 
-    const { tipo_de_header, set_tipo_de_header } = useGlobalContext();
+  const { tipo_de_header, set_tipo_de_header } = useGlobalContext();
 
   return (
 
-    <div>
-        <Header tipo={tipo_de_header} />
+    <div className="toda-a-tela-content">
+      <Header tipo={tipo_de_header} />
 
+      <div className="entre-navbar-e-footer-content">
+        <div className="perfil-brecho-content">
+
+          <div className="parte-azul-superior-content">
+            <p>a</p>
+          </div>
+
+          <div className="parte-do-meio-logo-nome-content">
+            <img src="" alt="" />
+            <div className="nome-brecho-e-avaliacao-content">
+              <p>Project Indigo Brechó</p>
+              <div className="avaliacao-simplificado-brecho-content">
+                <p>4.5/5</p>
+                <p>⭐⭐⭐⭐</p>
+              </div>
+
+            </div>
+          </div>
+
+          <div className="parte-inferior-do-perfil-brecho-content">
+            
+          </div>
+
+        </div>
+
+        <div className="produtos-do-brecho-content">
+
+        </div>
+
+        <div className="avaliacoes-do-brecho-content">
+
+        </div>
+
+      </div>
+
+      <Footer />
     </div>
 
   )
