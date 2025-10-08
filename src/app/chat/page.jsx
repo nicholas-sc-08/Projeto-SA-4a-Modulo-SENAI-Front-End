749b627c86be98cb237c5bf398aa2482742fddc4
nicholas-sc-08 <nicholassc.2008@gmail.com>
2025-10-08 15:11:03 -0300

    wip: chat

--- src/app/chat/page.jsx
+++ src/app/chat/page.jsx
@@ -5,11 +5,14 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { buscar_brechos } from "@/services/brecho/brecho";
 import { buscar_conversas, cadastrar_conversa } from "@/services/chat/chat";
-import styles from "@/app/chat/page.module.css";
 import { useGlobalContext } from "@/context/GlobalContext";
 import { buscar_clientes } from "@/services/cliente/cliente";
 import { AnimatePresence } from "framer-motion";
 import { motion } from "framer-motion";
+import { buscar_ultima_mensagem } from "@/services/chat/chat";
+import { nova_mensagem } from "@/services/chat/chat";
+import styles from "@/app/chat/page.module.css";
+import socket from "@/services/socket";
 
 export default function chat() {
 
@@ -25,7 +28,7 @@ export default function chat() {
     const { array_chat, set_array_chat } = useGlobalContext();
     const { array_clientes, set_array_clientes } = useGlobalContext();
     const { secao_chat, set_secao_chat } = useGlobalContext();
-    const { usuario_logado, set_usuario_logado } = useGlobalContext();
+    const { usuario_logado, set_usuario_logado } = useGlobalContext();  
     const dia_de_hoje = Date.now();
 
     useEffect(() => {
@@ -33,6 +36,14 @@ export default function chat() {
         buscar_brechos().then(data => set_array_brechos(data));
         buscar_conversas().then(mensagem => set_array_chat(mensagem));
         buscar_clientes().then(data => set_array_clientes(data));
+
+        socket.connect();
+        socket.on("receber_mensagem", mensagem => set_conversa_atual(mensagens_anteriores => [...mensagens_anteriores, mensagem]));
+        
+        return () => {
+
+            socket.off("receber_mensagem");
+        };
     }, []);
 
     useEffect(() => {
@@ -47,25 +58,6 @@ export default function chat() {
         };
 
     }, [pesquisa_inpt]);
-
-    function buscar_ultima_mensagem(_id) {
-
-        for (let i = array_chat.length - 1; i >= 0; i--) {
-
-            if (array_chat[i].id_dono_mensagem == _id && usuario_logado._id == array_chat[i].id_quem_recebeu_mensagem) {
-
-                return array_chat[i].mensagem;
-            };
-
-            if (array_chat[i].id_dono_mensagem == usuario_logado._id && array_chat[i].id_quem_recebeu_mensagem == _id) {
-
-                return array_chat[i].mensagem;
-            };
-
-        };
-
-        return `Nenhuma mensagem`;
-    };
 
     function selecionar_conversa(id) {
 
@@ -139,7 +131,7 @@ export default function chat() {
                             </aside>
                             <section className={styles["container_contato_info"]}>
                                 <h5>{conversa.nome || conversa.nome_brecho}</h5>
-                                <span>{buscar_ultima_mensagem(conversa._id)}</span>
+                                <span>{buscar_ultima_mensagem(conversa._id, array_chat, usuario_logado)}</span>
                             </section>
                         </div>
                     )) :
@@ -166,7 +158,12 @@ export default function chat() {
 
                         <main className={styles["container_mensagens"]}>
                             <div className={styles["container_mensagens_exibidas"]}>
+                                {conversa_atual && conversa_atual.length > 0 ? conversa_atual.map((_, i) => (
 
+                                    <div key={i} className={styles["container_historico_de_mensagens"]}>
+                                        
+                                    </div>
+                                )) : ``}
                             </div>
                             <footer className={styles["container_menu_interacao_conversa"]}>
                                 <div className={styles["container_menu_inpt_conversa"]}>
--- src/services/chat/chat.js
+++ src/services/chat/chat.js
@@ -8,65 +8,83 @@ export async function buscar_conversas() {
 
         const resposta = await api.get(`/chats`);
         return resposta.data;
-        
+
     } catch (erro) {
-      
+
         console.error(erro.message);
         throw new Error(`Erro ao buscar as conversas`);
     };
 };
 
-export async function buscar_conversa(id){
+export async function buscar_conversa(id) {
 
     try {
 
         const resposta = await api.get(`/chats/${id}`);
         return resposta.data;
-        
+
     } catch (erro) {
-      
+
         console.error(erro.message);
         throw new Error("Erro axios ao buscar a conversa pelo ID");
     };
 };
 
-export async function cadastrar_conversa(mensagem){
+export async function cadastrar_conversa(mensagem) {
 
     try {
 
         const resposta = await api.post(`/chats`, mensagem);
         return resposta.data;
-        
+
     } catch (erro) {
-        
+
         console.error(erro.message);
         throw new Error("Erro axios ao cadastrar a mensagem!");
     };
 };
 
-export async function atualizar_conversa(id, mensagem){
+export async function atualizar_conversa(id, mensagem) {
 
     try {
 
         const resposta = await api.put(`/chats/${id}`, mensagem);
         return resposta.data;
-        
+
     } catch (erro) {
-      
+
         console.error(erro.message);
         throw new Error("Erro axios ao atualizar a mensagem");
     };
 };
 
-export async function deletar_conversa(id){
+export async function deletar_conversa(id) {
 
     try {
 
         const reposta = await api.delete(`/chats/${id}`);
-        
+
     } catch (erro) {
-      
+
         console.error(erro.message);
         throw new Error("Erro axios ao deletar a mensagem pelo ID");
     };
+};
+
+export function buscar_ultima_mensagem(id, array_chat, usuario_logado) {
+
+    for (let i = array_chat.length - 1; i >= 0; i--) {
+
+        if (array_chat[i].id_dono_mensagem == id && usuario_logado._id == array_chat[i].id_quem_recebeu_mensagem) {
+
+            return array_chat[i].mensagem;
+        };
+
+        if (array_chat[i].id_dono_mensagem == usuario_logado._id && array_chat[i].id_quem_recebeu_mensagem == id) {
+
+            return array_chat[i].mensagem;
+        };
+    };
+
+    return `Nenhuma mensagem`;
 };
