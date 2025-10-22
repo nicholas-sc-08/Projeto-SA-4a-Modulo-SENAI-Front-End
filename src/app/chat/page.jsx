21cd1d691b001e7c2b61d0af0342e929ffcac90c
nicholas-sc-08 <nicholassc.2008@gmail.com>
2025-10-22 17:14:56 -0300

    fix: .trim

--- src/app/chat/page.jsx
+++ src/app/chat/page.jsx
@@ -43,6 +43,11 @@ export default function chat() {
         buscar_conversas().then(mensagem => set_array_chat(mensagem));
         buscar_clientes().then(data => set_array_clientes(data));
 
+                if (ref_final_conversa.current) {
+
+            ref_final_conversa.current.scrollIntoView({ behavior: "smooth" });
+        };
+
         socket.connect();
 
         function nova_mensagem(mensagem) {
@@ -76,10 +81,7 @@ export default function chat() {
 
     useEffect(() => {
 
-        if (ref_final_conversa.current) {
 
-            ref_final_conversa.current.scrollIntoView({ behavior: "smooth" });
-        };
     }, [conversa_atual]);
 
     useEffect(() => {
@@ -93,11 +95,11 @@ export default function chat() {
 
             if (cliente) {
 
-                const filtrar_conversas = usuario_logado.conversas.filter(conversa => conversa.nome_brecho.trim(` `).toUpperCase().includes(pesquisa_inpt.trim(` `).toUpperCase()));
+                const filtrar_conversas = usuario_logado.conversas.filter(conversa => conversa.nome_brecho.trim(``).toUpperCase().includes(pesquisa_inpt.trim(``).toUpperCase()));
                 set_array_de_pesquisa(filtrar_conversas);
             } else {
 
-                const filtrar_conversas = usuario_logado.conversas.filter(conversa => conversa.nome.trim(` `).toUpperCase().includes(pesquisa_inpt.trim(` `).toUpperCase()));
+                const filtrar_conversas = usuario_logado.conversas.filter(conversa => conversa.nome.trim(``).toUpperCase().includes(pesquisa_inpt.trim(``).toUpperCase()));
                 set_array_de_pesquisa(filtrar_conversas);
             };
         };
